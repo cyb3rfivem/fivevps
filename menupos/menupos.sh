dc0219549ccebd3d804552bec7bfcca8b171e6f3
cyb3rfivem <82467035+cyb3rfivem@users.noreply.github.com>
2021-04-14 18:27:45 -0300

    Update menupos.sh

--- menupos/menupos.sh
+++ menupos/menupos.sh
@@ -13,6 +13,9 @@ menu ()
  echo "= 3 - Inserir Resource na CFG        ="
  echo "= 4 - Sair do Menu                   ="
  echo "======================================"
+ echo "Digite a opção desejada:"
+ read firstop
+ echo "Opção selecionada: ($firstop)"
  
  case $firstop in
  
@@ -36,14 +39,14 @@ menu ()
 	 
 	 3)
 	  ##Inserir Resource
-	    cfglocate="localcfg"
-	    echo "Informe o caminho onde esta a cfg que você quer inserir!"
-	    echo "Exemplo: /home/ubuntu/FXServer/server/server-data/config "
-	    echo "ATENÇÃO! Não coloque / no final da ultima pasta         ^"
-	    echo "Criaremos um script onde você poderá executar no lugar deste menu!"
-	    echo "o Script ficara salvo na mesma pasta da CFG para facilitar a edição!"
-	    echo "Digite o caminho da cfg:"
-	    read cfglocate
+	  cfglocate="localcfg"
+	  echo "Informe o caminho onde esta a cfg que você quer inserir!"
+	  echo "Exemplo: /home/ubuntu/FXServer/server/server-data/config "
+	  echo "ATENÇÃO! Não coloque / no final da ultima pasta         ^"
+	  echo "Criaremos um script onde você poderá executar no lugar deste menu!"
+	  echo "o Script ficara salvo na mesma pasta da CFG para facilitar a edição!"
+	  echo "Digite o caminho da cfg:"
+	  read cfglocate
 	  ##CriandoScriptResourceNaCFG
       echo '#!bin/bash' > $cfglocate/rsedit.sh
       echo resourcename="nomeresource" >> $cfglocate/rsedit.sh
@@ -53,7 +56,7 @@ menu ()
       echo 'echo "Exemplo: resources.cfg"' >> $cfglocate/rsedit.sh
       echo read cfgname >> $cfglocate/rsedit.sh
       echo '' >> $cfglocate/rsedit.sh
-      echo menu () >> $cfglocate/rsedit.sh
+      echo 'menu ()' >> $cfglocate/rsedit.sh
       echo { >> $cfglocate/rsedit.sh
       echo while true do $firstop !="firstoption" >> $cfglocate/rsedit.sh
       echo do >> $cfglocate/rsedit.sh
@@ -63,15 +66,14 @@ menu ()
       echo 'echo "Exemplo: carros"' >> $cfglocate/rsedit.sh
       echo read resourcename >> $cfglocate/rsedit.sh
       echo '' >> $cfglocate/rsedit.sh
-      echo 'echo $resourcename >> $cfgname' >> $cfglocate/rsedit.sh
+      echo 'echo ensure $resourcename >> $cfgname' >> $cfglocate/rsedit.sh
       echo '' >> $cfglocate/rsedit.sh
-      echo esac >> $cfglocate/rsedit.sh
       echo done >> $cfglocate/rsedit.sh
       echo '' >> $cfglocate/rsedit.sh
       echo } >> $cfglocate/rsedit.sh
       echo menu >> $cfglocate/rsedit.sh
-	    echo "Script Criado com sucesso!!"
-	    sh $cfglocate/rsedit.sh
+	  echo "Script Criado com sucesso!!"
+	  sh $cfglocate/rsedit.sh
 	  exit;
 	 echo "======="
 	 ;;
